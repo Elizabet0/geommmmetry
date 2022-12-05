5d1252cbda451a1d02ba65830bdd79bde5a8465a
sirotinkinae.24 <sirotinkina.liza@gmail.com>
2022-12-05 14:42:47 +0300

    java doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import app.Application;
 import io.github.humbleui.jwm.App;
 
+/**
+ * Главный класс приложений
+ */
 public class Main {
+    /**
+     * Главный метод приложения
+     *
+     * @param args аргументы командной строки
+     */
 
     public static void main(String[] args) {
         App.start(Application::new);
