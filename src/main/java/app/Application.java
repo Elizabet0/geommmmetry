94267d2a2ca6fde5f15f7a9dd81c7e2e3627ffb3
sirotinkinae.24 <sirotinkina.liza@gmail.com>
2023-01-09 14:12:45 +0300

    panel created

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -21,6 +21,10 @@ public class Application implements Consumer<Event> {
      * окно приложения
      */
     private final Window window;
+    /**
+     * радиус скругления элементов
+     */
+    public static final int C_RAD_IN_PX = 4;
 
     /**
      * Конструктор окна приложения
